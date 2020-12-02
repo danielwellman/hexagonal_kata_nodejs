424a0e7581d258bb2f5b06a2605ac06e3a14ab39
Daniel Wellman <dan@danielwellman.com>
2020-12-02 15:49:34 -0600

    Rename sample CSV

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const FileSystemRepository = require("./lib/file_system_repository");
 const SmtpPostOffice = require("./lib/smtp_post_office");
 
 // Assemble the dependencies, using production adapters
-let repository = new FileSystemRepository('input.csv');
+let repository = new FileSystemRepository('employees_sample.csv');
 let postOffice = new SmtpPostOffice({
     serverAddress: "some-smtp-server.example.com",
     username: "...",
