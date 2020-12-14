0c6f811d20556f92bce8a198c781aac45eb65e5a
Daniel Wellman <dan@danielwellman.com>
2020-12-14 11:39:00 -0600

    Introduce factory method
    Ultimately I hope to use this as a place to move all I/O into so it can be asynchronous in just one part of the code (at the beginning of the program)

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const FileSystemEmployees = require("./lib/file_system_employees");
 const SmtpPostOffice = require("./lib/smtp_post_office");
 
 // Assemble the dependencies, using production adapters
-let employees = new FileSystemEmployees('employees_sample.csv');
+let employees = FileSystemEmployees.fromCsv('employees_sample.csv');
 let postOffice = new SmtpPostOffice({
     serverAddress: "some-smtp-server.example.com",
     username: "...",
